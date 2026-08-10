a4f4f38722c6942bad80aeb407217d64f0b5a74e
KhaledBaki <khaledbaki2018@gmail.com>
2026-08-10 13:09:15 -0400

    Added Basic syntax files

---
+++ General C Logic/BasicSyntax/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Basic print statement
+    printf("Hello world!\n");
+
+    // Basic for loop
+    for(int i = 0; i < 10; i++){
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
